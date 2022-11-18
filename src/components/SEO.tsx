3aa4f6391763d89764b78c2f2651f75f7d3d68b9
carlosestevesjr <carlosestevesjr0@gmail.com>
2022-11-18 11:17:29 -0300

    Alterando Description

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,4 @@
-import { Helmet  } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 export default function SEO({ title, description, name, type }: any) {
     return (
         <Helmet>
@@ -21,6 +21,15 @@ export default function SEO({ title, description, name, type }: any) {
             { /* Fonts tags */}
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@500&display=swap" rel="stylesheet"></link>
             { /* End Fonts tags */}
+
+            <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+            <meta http-equiv="cache-control" content="no-cache" />
+            <meta http-equiv="expires" content="0" />
+            <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+            <meta http-equiv="pragma" content="no-cache" />
+           
         </Helmet>
     )
 }
